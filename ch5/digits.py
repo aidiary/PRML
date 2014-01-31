e8e887088075c66fe475f272cf13c8041682cf84
sylvan5 <f2forest@gmail.com>
2014-01-31 14:01:50 +0900

    digitsデータをイメージで表示する関数を追加

--- ch5/digits.py
+++ ch5/digits.py
@@ -3,7 +3,6 @@ import numpy as np
 from mlp import MultiLayerPerceptron
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import LabelBinarizer
-#from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
 """
@@ -31,15 +30,32 @@ def train_test_split(X, y, test_rate=0.1):
 
     return X_train, X_test, y_train, y_test
 
+def draw_digits(digits):
+    """数字データの最初の10サンプルを描画"""
+    import pylab
+    for index, (image, label) in enumerate(zip(digits.images, digits.target)[:10]):
+        pylab.subplot(2, 5, index + 1)
+        pylab.axis('off')
+        pylab.imshow(image, cmap=pylab.cm.gray_r, interpolation='nearest')
+        pylab.title('%i' % label)
+    pylab.show()
+
 if __name__ == "__main__":
-    # scikit-learnの簡易数字データ
-    # Xサンプル, 8x8ピクセル
+    # scikit-learnの簡易数字データをロード
+    # 1797サンプル, 8x8ピクセル
     digits = load_digits()
+
+    # 数字データのサンプルを描画
+#     draw_digits(digits)
+
+    # 訓練データを作成
     X = digits.data
     y = digits.target
+    # ピクセルの値を0.0-1.0に正規化
     X /= X.max()
 
-    mlp = MultiLayerPerceptron(64, 100, 10)
+    # 多層パーセプトロン
+    mlp = MultiLayerPerceptron(64, 100, 10, act1="tanh", act2="sigmoid")
 
     # 訓練データ（90%）とテストデータ（10%）に分解
     X_train, X_test, y_train, y_test = train_test_split(X, y, 0.1)
