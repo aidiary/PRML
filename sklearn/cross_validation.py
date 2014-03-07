c77f7a72140a9c5a59483db45d23c323686b2857
sylvan5 <f2forest@gmail.com>
2014-03-07 17:01:45 +0900

    Cross-validationのサンプルを追加

---
+++ sklearn/cross_validation.py
@@ -0,0 +1,36 @@
+#coding: utf-8
+import numpy as np
+from sklearn import datasets, svm
+
+"""
+K-fold Cross-validation
+"""
+
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+
+K = 10
+
+# データをK分割する
+X_folds = np.array_split(X_digits, K)
+y_folds = np.array_split(y_digits, K)
+
+svc = svm.SVC(C=1, kernel='linear')
+
+scores = list()
+for k in range(K):
+    # k番目をテストセットとして残り2つで学習
+    # X_foldsは元のまま残しておくのでコピーする
+    X_train = list(X_folds)
+    X_test = X_train.pop(k)
+    X_train = np.concatenate(X_train)
+
+    y_train = list(y_folds)
+    y_test = y_train.pop(k)
+    y_train = np.concatenate(y_train)
+
+    scores.append(svc.fit(X_train, y_train).score(X_test, y_test))
+
+print scores
+print "Average:", np.average(scores)
---
+++ sklearn/cross_validation_easy.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+import numpy as np
+from sklearn import datasets, svm, cross_validation
+
+"""
+K-fold Cross-validation
+"""
+
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+
+K = 10
+
+svc = svm.SVC(C=1, kernel='linear')
+
+# KFoldは訓練集合とテスト集合の分割結果のインデックスを返す
+kfold = cross_validation.KFold(len(X_digits), n_folds=K)
+for i, (train_indices, test_indices) in enumerate(kfold):
+    print ('%d : Train: %s | test: %s' % (i, len(train_indices), len(test_indices)))
+
+# 分類精度を評価
+scores = cross_validation.cross_val_score(svc, X_digits, y_digits, cv=kfold, n_jobs=-1)
+
+print scores
+print "Average:", np.average(scores)
