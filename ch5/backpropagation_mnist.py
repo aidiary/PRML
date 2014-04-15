eb41e86b5a90bd90dc23fb2c244a7e5c4fde1dda
sylvan5 <f2forest@gmail.com>
2014-04-15 18:32:57 +0900

    コスト関数の計算を追加（途中）

--- ch5/backpropagation_mnist.py
+++ ch5/backpropagation_mnist.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import optimize
 from matplotlib import pyplot
 from sklearn.datasets import load_digits, fetch_mldata
+from sklearn.preprocessing import LabelBinarizer
 
 """
 多層パーセプトロンによる手書き文字認識
@@ -38,6 +39,12 @@ def randInitializeWeights(L_in, L_out):
     W = np.random.rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init
     return W
 
+def sigmoid(z):
+    return 1.0 / (1 + np.exp(-z))
+
+def safe_log(x, minval=0.0000000001):
+    return np.log(x.clip(min=minval))
+
 def J(nn_params, *args):
     """ニューラルネットのコスト関数"""
     in_size, hid_size, num_labels, X, y, lam = args
@@ -46,8 +53,34 @@ def J(nn_params, *args):
     Theta1 = nn_params[0:(in_size + 1) * hid_size].reshape((hid_size, in_size + 1))
     Theta2 = nn_params[(in_size + 1) * hid_size:].reshape((num_labels, hid_size + 1))
 
+    # 訓練データ数
+    m = X.shape[0]
+
+    # 訓練データの1列目にバイアス項に対応する1を追加
+    X = np.hstack((np.ones((m, 1)), X))
+
+    # 教師ラベルを1-of-K表記に変換
+    lb = LabelBinarizer()
+    lb.fit(y)
+    y = lb.transform(y)
+
+    J = 0
+    for i in range(m):
+        xi = X[i, :]
+        yi = y[i]
+        # forward propagation
+        a1 = xi
+        z2 = np.dot(Theta1, a1)
+        a2 = sigmoid(z2)
+        a2 = np.hstack((1, a2))
+        z3 = np.dot(Theta2, a2)
+        a3 = sigmoid(z3)
+        J += sum(-yi * safe_log(a3) - (1 - yi) * safe_log(1 - a3))
+    J /= m
+    return J
+
 if __name__ == "__main__":
-    input_layer_size = 400
+    input_layer_size = 64
     hidden_layer_size = 25
     num_labels = 10
 
@@ -75,5 +108,3 @@ if __name__ == "__main__":
     # 初期状態のコストを計算
     print "initial cost:", J(initial_nn_params, input_layer_size, hidden_layer_size,
                              num_labels, X, y, lam)
-
-
